a22c6c260364b29a263d1e5577c29d9b4e94812d
lovemxchine <lovemxchine@gmail.com>
2024-08-05 10:03:48 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import  React  from 'react';
+import './style.css'
+
+function App(){
+  const divStyle = {
+    color: 'blue' , 
+    backgroundColor : 'powderblue',
+    fontSize: 44,
+    padding: "3px"
+
+  }
+  return (
+    <>
+    <div style={divStyle}>Hello world</div>
+    <h1 className='title'> hello react nation</h1>
+    <h3>123</h3>
+    </>
+
+  )
+}
+export default App;
