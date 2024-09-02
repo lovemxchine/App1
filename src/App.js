6c5c61116aca5ebba33a992ef486500a68e96064
lovemxchine <lovemxchine@gmail.com>
2024-09-02 11:18:45 +0700

    add useContext(class and function component)

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import  React  from 'react';
+import  React , {useState}from 'react';
 import './style.css'
 // import Calendar from './class-component'
-import {Header, Content, Footer} from './func-component'  
+// import {Header, Content, Footer} from './func-component'  
+import {userContext} from './context'
+import Header2 from './context-header2'
+import Content2 from './context-content2'
 
 function App(){
+  let [user,setUser] = useState('')
   return (
-    <>
-        <Header/>
-        <Content/>
-        <Footer/>
-    </>
+    <userContext.Provider value={[user,setUser]} >
+        <Header2/>
+        <Content2/>
+
+    </userContext.Provider>
 
   )
 }
