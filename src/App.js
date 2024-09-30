e83e1b7b2c8bca359206ab18f7f7bfefd18ea8d1
lovemxchine <lovemxchine@gmail.com>
2024-09-30 11:10:20 +0700

    FormInputFile by Boostrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 
+import FormBootstrap from './form-bootstrap';
+import FormSearch from './form-search';
+import FormSelectFile from './form-select-file';
+import FormText from './form-text';
 import Router3 from './router3';
 
 function App(){
   return (
-    <Router3/>
+    <FormSelectFile/>
   )
 }
 export default App;
